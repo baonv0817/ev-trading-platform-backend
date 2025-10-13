c9b9ec51c18d3d6b8a0c9e250e6414664249ea0c
baonv0817 <babluokei4@gmail.com>
2025-10-13 07:44:16 +0700

    Initial commit

---
+++ src/main/java/com/fpt/evplatform/EvplatformApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.evplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/evplatform/EvplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.evplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
